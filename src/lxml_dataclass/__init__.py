bd1f82b4836a1b52c703e82bb3445c682b9a2df6
Sebastian Salinas <seba.salinas.delrio@gmail.com>
2025-05-01 12:39:05 -0400

    Added LxmlElement Reference and bumping version

--- src/lxml_dataclass/__init__.py
+++ src/lxml_dataclass/__init__.py
@@ -1,6 +1,7 @@
-from .element import Element as Element, element_field as element_field
+from .element import Element, LxmlElement, element_field
 
 __all__ = (
     "Element",
+    "LxmlElement",
     "element_field",
 )
